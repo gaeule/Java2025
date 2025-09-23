ce676757fd6fb59c0f06b473fc19ace001815603
SC <dmsgkwn123@gmail.com>
2025-09-23 10:47:44 +0900

    0923 수업실습

---
+++ CH05/src/sec01/exam01/VariableTest.java
@@ -0,0 +1,18 @@
+package sec01.exam01;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		int a = 0;	//null은 참조변수에만 사용가능
+		int b = a;
+		
+		String str1 = null;
+		String str2 = str1;
+		
+		System.out.println("Hi");
+
+//		int[] intArray = null;
+//		intArray[0] = 10;
+	}
+
+}
