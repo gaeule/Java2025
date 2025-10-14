a7a190cbc0ac6c2c57fe5915d0db9a883689ff1f
SeongChan Bae <dmsgkwn123@gmail.com>
2025-10-15 01:26:37 +0900

    손코딩

--- Ch06/src/sec01/exam01/Student.java
+++ Ch06/src/sec01/exam01/Student.java
@@ -1,20 +1,5 @@
 package sec01.exam01;
 
 public class Student {
-	int studentNumber;
-	String name;
-	double midScore;
-	double finalScore;
-	double projectScore;
-	
-	
-	public void SayHello()	{
-		System.out.println("Hello !");
-		
-		
-	}
-	public void SetMidScore ()
-	{
 
-	}
 }
