69d3770e2bd0561ed46eff00812b88f5756c3b95
SeongChan Bae <dmsgkwn123@gmail.com>
2025-10-30 01:00:51 +0900

    1028 수업실습

---
+++ Ch07/src/exam03/Animal.java
@@ -0,0 +1,14 @@
+package exam03;
+
+public class Animal {
+	boolean IsHealthy()
+	{
+		return true;
+	}
+	
+	void Heal()
+	{
+		
+	}
+
+}
---
+++ Ch07/src/exam03/NewHospital.java
@@ -0,0 +1,36 @@
+package exam03;
+
+public class NewHospital {
+
+	public static void main(String[] args) {
+		NewHospital h = new NewHospital();
+		
+		Dog dog = new Dog(100);
+		
+		h.CheckPatient(dog);
+		h.HealPatient(dog);
+		
+		Cat cat = new Cat(1);
+		
+		h.CheckPatient(cat);
+		h.HealPatient(cat);
+
+	}
+	
+	
+	void CheckPatient(Animal animal)
+	{
+		if(animal.IsHealthy())
+		{
+			System.out.println("healthy!");
+		} else
+		{
+			System.out.println("Sick!");
+		}
+	}
+	
+	void HealPatient(Animal animal)
+	{
+		animal.Heal();
+	}
+}
