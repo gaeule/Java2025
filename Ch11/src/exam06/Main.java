b973913ee98b2fe959297551e813e23a56400776
SC <dmsgkwn123@gmail.com>
2025-11-25 10:44:17 +0900

    1125 수업실습

---
+++ Ch11/src/exam06/Main.java
@@ -0,0 +1,14 @@
+package exam06;
+
+import exam05.Key;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SmartPhone phone1 = new SmartPhone("Samsung", "Anroid");
+		
+		System.out.println(phone1);
+
+	}
+
+}
