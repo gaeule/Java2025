aef8ab633fe08b44aa2833405786907d6189aa37
SeongChan Bae <dmsgkwn123@gmail.com>
2025-11-29 17:17:21 +0900

    상속 실습

---
+++ CH07/final04/Child.java
@@ -0,0 +1,12 @@
+package final04;
+
+public class Child extends Parent{
+	private int studentNo;
+	
+	public Child(String name, int studentNo) {
+		//this.name = name;
+		super(name);
+		this.studentNo = studentNo;
+	}
+
+}
